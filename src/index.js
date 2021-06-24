9659818c17fafe544312d4315cb0753c96023bca
Ajose Tosin <tosinemidastouch@gmail.com>
2021-06-24 22:24:02 +0100

    Starting robofriends project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+       <Card id={robots[0]} name={robots[0].name} email={robots[0].email}/>
+       <Card id={robots[1]} name={robots[1].name} email={robots[1].email}/>
+       <Card id={robots[2]} name={robots[2].name} email={robots[2].email}/>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

fad5b548186e06be262f21fd92d409aac4f6d4bb
Ajose Tosin <tosinemidastouch@gmail.com>
2021-06-24 22:27:46 +0100

    fix bug

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { robots } from './robots'
 

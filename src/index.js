d1d5ba859894c80710c4d8b4cd73fa66d466a4ea
Ajose Tosin <tosinemidastouch@gmail.com>
2021-06-25 12:03:25 +0100

    adding searchbox component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-       <Card id={robots[0]} name={robots[0].name} email={robots[0].email}/>
-       <Card id={robots[1]} name={robots[1].name} email={robots[1].email}/>
-       <Card id={robots[2]} name={robots[2].name} email={robots[2].email}/>
-    </div>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );

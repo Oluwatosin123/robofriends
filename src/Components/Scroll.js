a165d590ecfc491d83449a106809ce9e13538558
Ajose Tosin <tosinemidastouch@gmail.com>
2021-06-26 13:09:12 +0100

    Cod refactoring

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflow:'scroll', height:'800px'}}>
+        <div style={{overflowY:'scroll', height:'800px'}}>
             {props.children}
         </div>
     )
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll'
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
+import Scroll from '../Components/Scroll'
 import './App.css';
 
 
@@ -26,22 +26,20 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
+        return !robots.length ? <h1>Loading</h1> :
+            (
                 <div className='tc'>
                     <h1 className="f1 mb2">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                    <CardList robots={filteredRobots} />
+                        <CardList robots={filteredRobots} />
                     </Scroll>
                 </div>
             )
-        }
     }
 }
 

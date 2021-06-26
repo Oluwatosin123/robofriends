482ba04b4bfeda00c7e4c07dcaff64e576c28880
Ajose Tosin <tosinemidastouch@gmail.com>
2021-06-26 14:23:31 +0100

    added ErrorBoundry

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import Scroll from '../Components/Scroll'
+import ErrorBoundry from '../Components/ErrorBoundry';
 import './App.css';
 
 
@@ -36,7 +37,9 @@ class App extends Component {
                     <h1 className="f1 mb2">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
+                        <ErrorBoundry>
                         <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             )
